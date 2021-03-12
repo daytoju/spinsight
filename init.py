c0930aa36b97a1c59aa6d6227e907d8d96d7a6ea
Julien Sauvanet <jusauvanet@gmail.com>
2021-03-12 23:02:39 +0100

    update

--- init.py
+++ init.py
@@ -4,9 +4,19 @@ import dash_html_components as html
 
 app = dash.Dash(__name__)
 
-app.layout = html.Div(
-    dbc.Button("Success", color="success", className="mr-1")
-)
+app.layout = html.Div([
+    dbc.Row(
+        dbc.Col(
+            html.H1("----<===== SP Insight - JSA 2021 =====>----"),width={'size': 12, 'offset': 4}
+        )
+    ),
+
+    dbc.Row(
+        dbc.Col(
+            html.Div("Spectrum Protect Available Datas: "),width={'size': 6, 'offset': 1}
+        )
+    ),
+])
 
 if __name__ == "__main__":
     app.run_server()
